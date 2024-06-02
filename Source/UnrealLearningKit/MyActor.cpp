e43c2e354bd7d883377fdb31b6d24db459471d05
avantikar23 <152267983+avantikar23@users.noreply.github.com>
2024-06-02 22:24:05 +0530

    Module2

---
+++ Source/UnrealLearningKit/MyActor.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	StartLocation = GetActorLocation();
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	ToFroMotion(DeltaTime);
+	Rotator(DeltaTime);
+}
+void AMyActor:: Rotator(float DeltaTime){
+	FRotator CurrentRotation = GetActorRotation();
+	CurrentRotation += RotationSpeed*DeltaTime;
+	SetActorRotation(CurrentRotation);
+	if(!FullRotation){
+		if(CurrentRotation.Euler().Z - StartRotation.Euler().Z > MaxRotation || CurrentRotation.Euler().Z - StartRotation.Euler().Z < -MaxRotation){
+			RotationSpeed = FRotator(0,-RotationSpeed.Euler().Z,0);
+		}
+	}
+}
+
+void AMyActor::ToFroMotion(float DeltaTime){
+	FVector CurrentLocation;
+	CurrentLocation = GetActorLocation();
+	CurrentLocation += MovementSpeed*DeltaTime;
+	SetActorLocation(CurrentLocation);
+	DistanceMoved = FVector::Dist(StartLocation,CurrentLocation);
+	if(DistanceMoved >= MoveDistance)
+	{
+		FVector MoveDirection = MovementSpeed.GetSafeNormal();
+		StartLocation += MoveDistance*MoveDirection;
+		float offset = FVector::Dist(StartLocation,GetActorLocation());
+		SetActorLocation(StartLocation);
+		FString S = GetActorNameOrLabel();
+		UE_LOG(LogTemp,Warning,TEXT("Log printing  %f : %s"),offset,*S);
+		MovementSpeed = -MovementSpeed;
+	}
+}
+
+bool AMyActor::DestinationReached(FVector CurrentLocation) const
+{
+	float DistancekMoved = FVector::Dist(StartLocation,CurrentLocation);
+	return DistanceMoved >= MoveDistance;
+}
+
---
+++ Source/UnrealLearningKit/MyActor.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class UNREALLEARNINGKIT_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	FVector MovementSpeed = FVector(0,100,0);
+	UPROPERTY(EditAnywhere)
+	float DistanceMoved;
+	UPROPERTY(EditAnywhere)
+	float MoveDistance = 500;
+	FVector StartLocation;
+	void ToFroMotion(float DeltaTime);
+	bool DestinationReached(FVector CurrentLocation) const;
+	UPROPERTY(EditAnywhere)
+	FRotator RotationSpeed = FRotator(0,0,0);
+	UPROPERTY(EditAnywhere)
+	float MaxRotation;
+	UPROPERTY(EditAnywhere)
+	bool FullRotation;
+	void Rotator(float DeltaTime);
+    FRotator StartRotation;
+};
